1b616a7773c730c7fe0372437c9a30b948a31534
Artur Drabinko <artur.drabinko@gmail.com>
2018-01-24 13:28:51 +0300

    Init

---
+++ C#/Application/Application/Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Application
+{
+    public class Patient
+    {
+        public string name;
+        public string service;
+        public int cost;
+
+        public Patient()
+        {
+            name = "-";
+            service = "-";
+            cost = 0;
+        }
+
+        public Patient(string _name, string _service, int _cost)
+        {
+            name = _name;
+            service = _service;
+            cost = _cost;
+        }
+
+
+    }
+
+    public class Database
+    {
+        List<Patient> listPatients = new List<Patient>();
+
+        public void pause()
+        {
+            Console.Write("\n\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        public void printAllPatient()
+        {
+            Console.WriteLine("==================================================================");
+            Console.WriteLine("| " + "№".PadLeft(4) + " | " + "FIO patient".PadLeft(22) + " | " + "Service".PadLeft(16) + " | " + "Cost".PadLeft(11) + " |");
+            Console.WriteLine("| " + " ".PadLeft(4) + " | " + " ".PadLeft(22) + " | " + " ".PadLeft(16) + " | " + " ".PadLeft(11) + " |");
+            Console.WriteLine("==================================================================");
+            int countPatients = 1;
+            foreach (Patient pacient in listPatients)
+            {
+                Console.WriteLine("| " + countPatients.ToString().PadLeft(4) + " | " + pacient.name.PadLeft(22) + " | " + pacient.service.PadLeft(16) + " | " + pacient.cost.ToString().PadLeft(11) + " |");
+                Console.WriteLine("==================================================================");
+                countPatients++;
+            }
+        }
+
+        public bool viewAllPatients()
+        {
+            if (listPatients.Count != 0)
+            {
+                printAllPatient();
+                return true;
+            }
+            else return false;
+        }
+
+
+        public bool addNewPatientInDatabase(Patient _patient)
+        {
+            if (_patient.name != "-" && _patient.service != "-")
+            {
+                listPatients.Add(_patient);
+                return true;
+            }
+            else return false;
+
+        }
+
+        public bool deletePatientByNumber(string number)
+        {
+            int countPatients = 1;
+            foreach (Patient pacient in listPatients)
+            {
+                if (countPatients.ToString() == number)
+                {
+                    listPatients.Remove(pacient);
+                    return true;
+                }
+                countPatients++;
+            }
+            return false;
+        }
+
+    }
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int enterNumber;
+            string command;
+            Database database = new Database();
+
+            Patient patient1 = new Patient("Sidorov A.S.", "Massage", 20);
+            Patient patient2 = new Patient("Sidorov A.S.", "Massage", 22);
+            Patient patient3 = new Patient("Sidorov A.S.", "Yzi", 4);
+            Patient patient4 = new Patient("Sidorov A.S.", "Blood analize", 67);
+            Patient patient5 = new Patient("Sidorov A.S.", "Massage", 12);
+
+            database.addNewPatientInDatabase(patient1);
+            database.addNewPatientInDatabase(patient2);
+            database.addNewPatientInDatabase(patient3);
+            database.addNewPatientInDatabase(patient4);
+            database.addNewPatientInDatabase(patient5);
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\n-----User menu-----\n");
+                Console.WriteLine("1.View all patients");
+                Console.WriteLine("2.Add new patient");
+                Console.WriteLine("3.Remove patient");
+                Console.WriteLine("0.Exit\n");
+
+                Console.Write("\nEntry menu number: ");
+                try {
+                    enterNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException) {
+                    enterNumber = 404;
+                }
+
+            
+                switch (enterNumber)
+                {
+                    case 1:
+                        Console.Clear(); Console.WriteLine("\n----List all patients----\n\n");
+                        if (!database.viewAllPatients())
+                        {
+                            Console.Write("\nError!\nList patients is empty!\n");
+                        }
+
+                        database.pause();
+                        break;
+                    case 2:
+                        Console.Clear(); Console.Write("\n-----Adding new patient-----\n\n");
+
+                        string name;
+                        string service;
+                        int cost;
+
+                        Console.Write("\nEntry name - "); name = Convert.ToString(Console.ReadLine());
+                        Console.Write("\nEntry service name - "); service = Convert.ToString(Console.ReadLine());
+                        try
+                        {
+                            Console.Write("\nEntry service cost - "); cost = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (FormatException)
+                        {
+                            Console.Clear();
+                            Console.Write("\n\n-----Error!-----\n");
+                            Console.Write("\nThe cost of the service can not contain string characters!\n");
+                            Console.Write("Try again!\n\n");
+                            database.pause();
+                            return;
+                        }
+
+                        Patient patient = new Patient(name, service, cost);
+
+                        Console.WriteLine("\n\nDo you really want to add a new patient?\n");
+
+                        Console.Write("\nAdd new patient (y/n) - "); command = Convert.ToString(Console.ReadLine());
+                        if (command == "y")
+                        {
+                            if (database.addNewPatientInDatabase(patient)) Console.WriteLine("\n\nThe patient was successfully added \nto the database!\n\n");
+                            else Console.WriteLine("\n\nError adding patient to database!\n\n");
+                        }
+                        else Console.WriteLine("\n\nCancellation of patient addition!\n\n");
+
+                        database.pause();
+                        break;
+                    case 3:
+                        string patientNumberForRemove; Console.Clear();
+                        Console.Write("\n-----Removing the patient from the database-----\n\n");
+                        database.printAllPatient();
+
+                        Console.Write("\nEntry number patient for remove - "); patientNumberForRemove = Convert.ToString(Console.ReadLine());
+
+                        Console.Write("\nRemove this patient (y/n) - "); command = Convert.ToString(Console.ReadLine());
+                        if (command == "y")
+                        {
+                            if (database.deletePatientByNumber(patientNumberForRemove))
+                            {
+                                Console.Write("\nThe patient was successfully removed!\n\n");
+                            }
+                            else
+                            {
+                                Console.Write("\nA patient with such a number is not listed!\n\n");
+                            }
+                            database.pause();
+                        }
+                        else
+                        {
+                            Console.Write("\nUndo delete!\n\n");
+                            database.pause();
+                        }
+                        break;
+                    case 0:
+                        Console.WriteLine("\n\nPress any key to continue...");
+                        Console.ReadKey();
+                        return;
+                    default:
+                        Console.WriteLine("\n\nThere is no such command in the menu!");
+                        Console.WriteLine("Try again!\n\n");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
+            } 
+
+
+        }
+
+    }
+}
---
+++ C#/Application/ApplicationTests/DatabaseTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tests
+{
+    [TestClass()]
+    public class DatabaseTests
+    {
+
+        [TestMethod()]
+        public void UserConstructorAllParamTest()
+        {
+            
+            //arrange Вводи входные значени и значения которые ожидаем получить
+            string input_name = "Ibragimov K.V.";
+            string input_servece = "Massage";
+            int input_cost = 55;
+
+            string expected_name = "Ibragimov K.V.";
+            string expected_servece = "Massage";
+            int expected_cost = 55;
+
+            Patient patient = new Patient(input_name, input_servece, input_cost);
+
+
+            //act Получаем актуальные значения
+            string actual_name = patient.name;
+            string actual_servece = patient.service;
+            int actual_cost = patient.cost;
+
+
+            //assert Проверяем при помощи класса Assert
+            Assert.AreEqual(expected_name, actual_name, "Patient name {0} should have been {1}!", actual_name, expected_name);
+            Assert.AreEqual(expected_servece, actual_servece, "Service {0} should have been {1}!", actual_servece, expected_servece);
+            Assert.AreEqual(expected_cost, actual_cost, "Cost {0} should have been {1}!", actual_cost, expected_cost);
+        }
+
+
+        [TestMethod()]
+        public void addNewPatientInDBCorrectParamTest()
+        {
+            Patient patient = new Patient("Petrov G.V.", "Massage", 40);
+
+            Database database = new Database();
+
+            //Проверяем добавился ли наш пациент в БД.
+            Assert.IsTrue(database.addNewPatientInDatabase(patient));
+        }
+
+
+        [TestMethod()]
+        public void addNewPatientInDBWrongParamTest()
+        {
+            Patient patient = new Patient();
+
+            Database database = new Database();
+
+            //Не указали информацию о пользователе, ожидаем ошибку добавления.
+            Assert.IsFalse(database.addNewPatientInDatabase(patient));
+        }
+
+
+        [TestMethod()]
+        public void deletePatientByNumberFromDBTest()
+        {
+            Patient patient = new Patient("Petrov G.V.", "Massage", 40);
+
+            Database database = new Database();
+            database.addNewPatientInDatabase(patient);
+
+            //В базе только 1 пользователь удаление должно пройти успешно
+            Assert.IsTrue(database.deletePatientByNumber("1"));
+        }
+
+        [TestMethod()]
+        public void deletePatientByNumberFromEmptyDBTest()
+        {
+            Patient patient = new Patient("Petrov G.V.", "Massage", 40);
+
+            Database database = new Database();
+
+            //В базе не пользователей удаление не произдет должно прийти значение false
+            Assert.IsFalse(database.deletePatientByNumber("1"));
+        }
+
+        [TestMethod()]
+        public void viewAllPatientsEmptyListTest()
+        {
+            Database database = new Database();
+
+            //В базе не пользователей при просмотре должно прийти значение false
+            Assert.IsFalse(database.viewAllPatients());
+        }
+
+        [TestMethod()]
+        public void viewAllPatientsNotEmptyListTest()
+        {
+            Database database = new Database();
+            Patient patient = new Patient("Petrov G.V.", "Yzi", 40);
+
+            database.addNewPatientInDatabase(patient);
+
+            //В базе есть пользователь результат просмотра должен пройти успешно
+            Assert.IsTrue(database.viewAllPatients());
+        }
+
+
+    }
+}
